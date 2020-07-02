90891e5fd3f9bc3222a44210da416a90acfe5b51
Piotr Szulc <szulo123@gmail.com>
2020-07-02 16:07:21 +0200

    Fetching weather data from API

--- include/OpenWeatherClient.h
+++ include/OpenWeatherClient.h
@@ -1,6 +1,10 @@
 #ifndef OPENWEATHERCLIENT_H
 #define OPENWEATHERCLIENT_H
 
+#include <HTTPClient.h>
+
+const char _currentWeatherUri[] = "https://api.openweathermap.org/data/2.5/weather?id={cityId}&appid={apiKey}&units=metric";
+
 class OpenWeatherClient {
     public:
         OpenWeatherClient(int cityId, const char* apiKey);
@@ -10,6 +14,7 @@ class OpenWeatherClient {
     private:
         int _cityId;
         const char* _apiKey;
+        String getCurrentUri();
 };
 
 #endif
--- src/OpenWeatherClient.cpp
+++ src/OpenWeatherClient.cpp
@@ -6,6 +6,26 @@ OpenWeatherClient::OpenWeatherClient(int cityId, const char* apiKey) : _cityId(c
 void OpenWeatherClient::fetchCurrentWeather() {
 }
 
+String OpenWeatherClient::getCurrentUri() {
+    String uri = String(_currentWeatherUri);
+    uri.replace("{cityId}", String(_cityId));
+    uri.replace("{apiKey}", _apiKey);
+    return uri;
+}
+
 float OpenWeatherClient::getCurrentTemp() {
+    String url = getCurrentUri();
+
+    HTTPClient client;
+    client.begin(url);
+
+    int code = client.GET();
+    Serial.println(code);
+
+    if (code > 0) {
+        String payload = client.getString();
+        Serial.println(payload);
+    }
+
     return 26.7;
 }
